94f1e710737eded571f7f3b472deb8074cde582e
Piyush Joshi <pjoshimail@gmail.com>
2017-11-07 13:27:47 -0800

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -2,9 +2,11 @@ class JenkinsDemo
 {
 public static void main(String[] args)
 {
+  System.out.println("Welocome from Jenkins");
 for (int i=1;i<=8;i++)
 {
 System.out.println("Hello Jenkins"+i);
 }
+  
 }
 }
